2ec495d8fbc32d5e4bee2edb592622d5b1976316
WaleedMohamed0 <91630222+WaleedMohamed0@users.noreply.github.com>
2022-02-15 17:07:22 +0200

    Deleted Not Used Files

--- lib/modules/cubit/states.dart
+++
@@ -1,6 +0,0 @@
-abstract class AppStates {}
-class AppInitialState extends AppStates{}
-class SuccessAppState extends AppStates{}
-class ChangePassVisibilityState extends AppStates{}
-class ErrorAppState extends AppStates{}
-class ChangeAppMode extends AppStates{}
